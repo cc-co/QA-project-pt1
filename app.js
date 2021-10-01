d7ef3fc7e3a6a81f4eb3dcf495b33634c2459684
cc-co <cwychan@hotmail.co.uk>
2021-10-01 10:38:19 +0100

    add post

--- app.js
+++ app.js
@@ -11,6 +11,20 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
+productAdd = (productName, productDes, productPrice) => {
+    
+    let product = {
+        name: productName,
+        description: productDes,
+        price: productPrice
+    };
+    
+    return product;
+
+};
+
+// GET - RESTful 
+
 app.get('/products', (req, res) => {
 
     console.log(`Reading all products: \n`);
@@ -26,6 +40,30 @@ app.get('/products', (req, res) => {
 
 });
 
+// POST
+
+app.post('/product/create', (req, res) => {
+
+    console.log(`Creating product: \n`);
+
+    let product = productAdd(req.body.name, req.body.description, req.body.price);
+
+    db.insert(product, (err, product) => {
+
+        if (err) res.send(err);
+
+       res.status(200).send(product);
+
+        console.log(product);
+    });
+
+});
+
+
+
 app.listen(PORT, () => {
     console.log(`API listening on http:localhost:${PORT}`);
 });
+
+
+
